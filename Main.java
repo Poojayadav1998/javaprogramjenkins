e695c4525beedc321080fcdfa80c0f1a86829234
pooja <poojayadav2305@gmail.com>
2025-10-16 17:44:04 +0530

    Added Student

--- Main.java
+++ Main.java
@@ -7,5 +7,8 @@ public class Main{
         System.out.println(new Date());
         System.out.println("hello");
         System.out.println("added one more file");
+
+        Student s = new Student();
+        s.sayHello();
     }
 }
